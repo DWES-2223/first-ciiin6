8db6377834e0c6aea03d5ffd5ea06651b3da0742
Cintia Cantó <cincanest@alu.edu.gva.es>
2023-10-02 14:11:33 +0200

    [ADD] Activitats 270 - 272

---
+++ php/src/270a.php
@@ -0,0 +1,9 @@
+<tr>
+        <td><?= $prova ?></td>
+        <td><?= $record["marca"] ?></td>
+        <td><?= $record["atleta"] ?></td>
+        <td><?= $record["natalici"] ?></td>
+        <td><?= $record["club"] ?></td>
+        <td><?= $record["data"] ?></td>
+        <td><?= $record["lloc"] ?></td>
+</tr>
